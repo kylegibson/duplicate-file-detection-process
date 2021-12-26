bae59a2a3239b3997143f76926573ef523419c01
Kyle Gibson <kyle.gibson@frozenonline.com>
2021-12-26 13:23:42 -0500

    quote path to deal with spaces

--- step_1_generate_hash_report.sh
+++ step_1_generate_hash_report.sh
@@ -2,4 +2,4 @@ hash_file() {
   dd if="$1" bs=1024 count=16 2>/dev/null | sha224sum | cut -f1 -d" " | xargs printf "%s\t$1\n"
 }
 export -f hash_file
-find $1 -type f -print0 | xargs -0 -L1 -P8 -I'^' bash -c 'hash_file "^"'
+find "$1" -type f -print0 | xargs -0 -L1 -P8 -I'^' bash -c 'hash_file "^"'
